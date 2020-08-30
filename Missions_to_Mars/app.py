6b1c8654b61f183c738e8138a3e9c32517a97ce0
SAS 826 <sasotillo826@gmail.com>
2020-08-29 23:23:19 -0400

    updating hw so far

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -23,12 +23,12 @@ def home():
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
-
+    mars = mongo.db.mars
     # Run the scrape function
     mars_data = scrape_mars.scrape_info()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    mongo.db.collection.update({}, mars, upsert=True)
 
     # Redirect back to home page
     return redirect("/")

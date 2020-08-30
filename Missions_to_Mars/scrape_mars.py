b32f1583548851094a7f063486cb8fdb10807be3
SAS 826 <sasotillo826@gmail.com>
2020-08-30 18:38:24 -0400

    almost done with HW, just have to master scraped Table

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -15,7 +15,7 @@ def scrape_info():
 
     url = "https://mars.nasa.gov/news"
     browser.visit(url)
-    time.sleep(5)
+    time.sleep(10)
     html = browser.html
     news_soup = bs(html, 'html.parser')
     slide_elem = news_soup.select_one('ul.item_list li.slide')
@@ -24,6 +24,7 @@ def scrape_info():
 
     url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
     browser.visit(url)
+    time.sleep(15)
     full_image_elem = browser.find_by_id('full_image')
     full_image_elem.click()
     more_info_element = browser.links.find_by_partial_text('more info')
@@ -35,6 +36,7 @@ def scrape_info():
 
     url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(url)
+    time.sleep(10)
     mars_pic1 = browser.links.find_by_partial_text('Cerberus')
     mars_pic1.click()
     mars_pic1o = browser.links.find_by_partial_text('Open')
@@ -46,6 +48,7 @@ def scrape_info():
 
     url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(url)
+    time.sleep(10)
     mars_pic2 = browser.links.find_by_partial_text('Schiaparelli')
     mars_pic2.click()
     mars_pic2o = browser.links.find_by_partial_text('Open')
@@ -57,6 +60,7 @@ def scrape_info():
     
     url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(url)
+    time.sleep(10)
     mars_pic3 = browser.links.find_by_partial_text('Syrtis')
     mars_pic3.click()
     mars_pic3o = browser.links.find_by_partial_text('Open')
@@ -68,6 +72,7 @@ def scrape_info():
 
     url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(url)
+    time.sleep(10)
     mars_pic4 = browser.links.find_by_partial_text('Valles')
     mars_pic4.click()
     mars_pic4o = browser.links.find_by_partial_text('Open')
@@ -77,6 +82,15 @@ def scrape_info():
     img4_title = img_soup.find("h2", class_="title").text
     img4_url = img_soup.find("li").a['href']
 
+    url = "https://space-facts.com/mars/"
+    browser.visit(url)
+    time.sleep(10)
+    tables = pd.read_html(url)
+    df = tables[0]
+    df.columns = ['Planet Profile:', 'Mars (The Red Planet)']
+    df.set_index('Planet Profile:', inplace=True)
+    html_table_n = df.to_html()
+    html_table = html_table_n.replace('\n', '')
 
     # Storing data in my dictionary
     mars_data = {
@@ -91,6 +105,7 @@ def scrape_info():
         "img3_url": img3_url,
         "img4_title": img4_title,
         "img4_url": img4_url,
+        "html_table": html_table,
     }
 
     # Closing the browser after scraping

d6a6541cedefa312d4c5b7fd340bcdbb7294d48d
SAS 826 <sasotillo826@gmail.com>
2020-08-30 15:44:27 -0400

    HW so far

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -5,56 +5,101 @@ import time
 
 
 def init_browser():
-    # @NOTE: Replace the path with your actual path to the chromedriver
     executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
     return Browser("chrome", **executable_path, headless=False)
 
 
 def scrape_info():
+    # Opening the browser
     browser = init_browser()
 
-    # Visit visitcostarica.herokuapp.com
     url = "https://mars.nasa.gov/news"
     browser.visit(url)
-
     time.sleep(1)
-
-    # Scrape page into Soup
     html = browser.html
     news_soup = bs(html, 'html.parser')
-
     slide_elem = news_soup.select_one('ul.item_list li.slide')
-
     news_title = slide_elem.find('div', class_='content_title').text
-
     news_p = slide_elem.find('div', class_='article_teaser_body').text
 
     url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
     browser.visit(url)
-
+    time.sleep(1)
     full_image_elem = browser.find_by_id('full_image')
     full_image_elem.click()
-
     more_info_element = browser.links.find_by_partial_text('more info')
     more_info_element.click()
-
     html = browser.html
     img_soup = bs(html, 'html.parser')
-
     img_url_rel = img_soup.select_one('figure.lede a img').get('src')
-
     featured_image_url = f'https://www.jpl.nasa.gov{img_url_rel}'
 
-    
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+    time.sleep(1)
+    mars_pic1 = browser.links.find_by_partial_text('Cerberus')
+    mars_pic1.click()
+    mars_pic1o = browser.links.find_by_partial_text('Open')
+    mars_pic1o.click()
+    html = browser.html
+    img_soup = bs(html, 'html.parser')
+    img1_title = img_soup.find("h2", class_="title").text
+    img1_url = img_soup.find("li").a['href']
 
-    # Store data in a dictionary
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+    time.sleep(1)
+    mars_pic2 = browser.links.find_by_partial_text('Schiaparelli')
+    mars_pic2.click()
+    mars_pic2o = browser.links.find_by_partial_text('Open')
+    mars_pic2o.click()
+    html = browser.html
+    img_soup = bs(html, 'html.parser')
+    img2_title = img_soup.find("h2", class_="title").text
+    img2_url = img_soup.find("li").a['href']
+    
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+    time.sleep(1)
+    mars_pic3 = browser.links.find_by_partial_text('Syrtis')
+    mars_pic3.click()
+    mars_pic3o = browser.links.find_by_partial_text('Open')
+    mars_pic3o.click()
+    html = browser.html
+    img_soup = bs(html, 'html.parser')
+    img3_title = img_soup.find("h2", class_="title").text
+    img3_url = img_soup.find("li").a['href']
+
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+    time.sleep(1)
+    mars_pic4 = browser.links.find_by_partial_text('Valles')
+    mars_pic4.click()
+    mars_pic4o = browser.links.find_by_partial_text('Open')
+    mars_pic4o.click()
+    html = browser.html
+    img_soup = bs(html, 'html.parser')
+    img4_title = img_soup.find("h2", class_="title").text
+    img4_url = img_soup.find("li").a['href']
+
+
+
+    # Storing data in my dictionary
     mars_data = {
         "news_title": news_title,
         "news_p": news_p,
         "featured_image_url": featured_image_url,
+        "img1_title": img1_title,
+        "img1_url": img1_url,
+        "img2_title": img2_title,
+        "img2_url": img2_url,
+        "img3_title":img3_title,
+        "img3_url": img3_url,
+        "img4_title": img4_title,
+        "img4_url": img4_url,
     }
 
-    # Close the browser after scraping
+    # Closing the browser after scraping
     browser.quit()
 
     # Return results
